369b23af311760661e042526f693c3a9a36c8b5f
ingridD2707 <im938407@gmail.com>
2025-12-02 13:26:14 +0100

    feat (main):first commit

---
+++ main.py
@@ -0,0 +1,91 @@
+import time
+
+def calculate_fare(seconds_stopped, seconds_moving):
+    """
+    función para calcular la tarifa total en euros
+    stopped:0.02€/s moving:0.05€/s
+    """
+    fare = seconds_stopped * 0.02 + seconds_moving * 0.05
+    print(f"Este es el total: {fare}€")
+    return fare
+
+
+def taximeter():
+    print("WELCOME TO THE TAXIMETER")
+    print("Available commands: 'start', 'stop', 'move', 'finish', 'exit'\n")
+
+    trip_activate = False
+    stopped_time = 0
+    moving_time = 0
+    state = None
+    state_start_time = 0
+
+    while True:
+        command = input("> ").strip().lower()
+
+        
+        if command == "start":
+            if trip_activate:
+                print("Error: a trip is already in progress.")
+                continue
+
+            trip_activate = True
+            stopped_time = 0
+            moving_time = 0
+            state = "stopped"
+            state_start_time = time.time()
+            print("Trip started. Initial state: 'stopped'")
+
+        
+        elif command in ("stop", "move"):
+            if not trip_activate:
+                print("Error: No active trip. Please start first.")
+                continue
+
+            duration = time.time() - state_start_time
+
+            if state == "stopped":
+                stopped_time += duration
+            else:
+                moving_time += duration
+
+            state = "stopped" if command == "stop" else "moving"
+            state_start_time = time.time()
+            print(f"State changed to '{state}'.")
+
+        # FINISH
+        elif command == "finish":
+            if not trip_activate:
+                print("Error: No active trip to finish.")
+                continue
+
+            duration = time.time() - state_start_time
+
+            if state == "stopped":
+                stopped_time += duration
+            else:
+                moving_time += duration
+
+            print("\n--- Trip Summary ---")
+            print(f"Stopped time: {stopped_time:.1f} seconds")
+            print(f"Moving time: {moving_time:.1f} seconds")
+
+            total_fare = calculate_fare(stopped_time, moving_time)
+            print(f"Total fare: €{total_fare:.2f}")
+            print("---------------------\n")
+
+            trip_activate = False
+            state = None
+
+       
+        elif command == "exit":
+            print("Exiting the program, goodbye!")
+            break
+
+        
+        else:
+            print("Unknown command. Use: 'start', 'stop', 'move', 'finish', or 'exit'")
+
+
+if __name__ == "__main__":
+    taximeter()
